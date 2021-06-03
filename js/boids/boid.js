9218819ef2e96751271d9c426ed8c5cd4d15130d
Andwerpz <andwerpooni@gmail.com>
2021-06-03 12:23:47 -0500

    Optimizations for the js banner

--- js/boids/boid.js
+++ js/boids/boid.js
@@ -1,11 +1,11 @@
 
 class Boid {
-	constructor() {
-		this.position = createVector(random(width), random(height));
+	constructor(x, y, pr) {
+		this.position = createVector(x, y);
 		this.velocity = p5.Vector.random2D();
 		this.velocity.mult(random(4));
 		this.acceleration = createVector();
-		this.perceptionRadius = 50;
+		this.perceptionRadius = pr;
 		this.maxForce = 0.2;
 		this.maxSpeed = 2;
 	}
@@ -17,9 +17,23 @@ class Boid {
 	}
 	
 	tick() {
+	
+		/*let bucketXLength = bucket.length;
+		let bucketYLength = bucket[0].length;
+	
+		let bucketX = (bucket.length + parseInt(Math.min(this.position.x / this.perceptionRadius, (width / this.perceptionRadius) - 1))) % bucketXLength;
+		let bucketY = (bucket[0].length + parseInt(Math.min(this.position.y / this.perceptionRadius, (height / this.perceptionRadius) - 1))) % bucketYLength;
+		for(let i = 0; i < bucket[bucketX][bucketY].length; i++){
+			if(bucket[bucketX][bucketY][i] == this){
+				//print("REMOVE");
+				bucket[bucketX][bucketY].splice(i, 1);
+				break;
+			}
+		}*/
 		this.position.add(this.velocity);
 		this.velocity.add(this.acceleration);
 		this.velocity.limit(this.maxSpeed);
+		
 		if(this.position.x > width){
 			this.position.x -= width;
 		}
@@ -32,82 +46,96 @@ class Boid {
 		if(this.position.y < 0){
 			this.position.y += height;
 		}
+		/*bucketX = (bucket.length + parseInt(Math.min(this.position.x / this.perceptionRadius, (width / this.perceptionRadius) - 1))) % bucketXLength;
+		bucketY = (bucket[0].length + parseInt(Math.min(this.position.y / this.perceptionRadius, (height / this.perceptionRadius) - 1))) % bucketYLength;
+		bucket[bucketX][bucketY].push(this);*/
+		//print("ADD");
 	}
 	
-	align(boids){
-		let avg = createVector();
+	flock(bucket){
+		
+		let bucketXLength = bucket.length;
+		let bucketYLength = bucket[0].length;
+		
+		let avgDiff = createVector();
+		let avgPos = createVector();
+		let avgVel = createVector();
 		let count = 0;
-		for(let b of boids){
-			let d = dist(this.position.x, this.position.y, b.position.x, b.position.y);
-			if(b != this && d <= this.perceptionRadius){
-				avg.add(b.velocity);
-				count ++;
+		let bucketX = bucket.length + parseInt(Math.min(this.position.x / this.perceptionRadius, (width / this.perceptionRadius) - 1));
+		let bucketY = bucket[0].length + parseInt(Math.min(this.position.y / this.perceptionRadius, (height / this.perceptionRadius) - 1));
+		for(let i = bucketX - 1; i <= bucketX + 1; i++){
+			for(let j = bucketY - 1; j <= bucketY + 1; j++){
+				for(let test of bucket[i % bucketXLength][j % bucketYLength]){
+					let b = createVector(test.position.x, test.position.y);
+					if(dist(this.position.x, this.position.y, b.x + width, b.y) < dist(this.position.x, this.position.y, b.x, b.y)){
+						b.x += width;
+					}
+					else if(dist(this.position.x, this.position.y, b.x - width, b.y) < dist(this.position.x, this.position.y, b.x, b.y)){
+						b.x -= width;
+					}
+					if(dist(this.position.x, this.position.y, b.x, b.y + height) < dist(this.position.x, this.position.y, b.x, b.y)){
+						b.y += height;
+					}
+					else if(dist(this.position.x, this.position.y, b.x, b.y - height) < dist(this.position.x, this.position.y, b.x, b.y)){
+						b.y -= height;
+					}
+					let d = dist(this.position.x, this.position.y, b.x, b.y);
+					if(test != this && d <= this.perceptionRadius){
+						count ++;
+						
+						//alignment
+						avgVel.add(test.velocity);
+						
+						//cohesion
+						avgPos.add(b);
+					
+						//separation
+						let diff = p5.Vector.sub(this.position, b);
+						diff.div(d * d);
+						avgDiff.add(diff);
+						strokeWeight(Math.min((100 * this.perceptionRadius) / (d * d), 1));
+						line(this.position.x, this.position.y, b.x, b.y);
+					}
+				}
 			}
 		}
+		
 		if(count != 0){
-			avg.div(count);
-			avg.setMag(this.maxSpeed);
-			avg.sub(this.velocity);	
-			avg.limit(this.maxForce);
+		
+			//alignment
+			avgVel.div(count);
+			avgVel.setMag(this.maxSpeed);
+			avgVel.sub(this.velocity);	
+			avgVel.limit(this.maxForce);
+		
+			//cohesion
+			avgPos.div(count);
+			avgPos.sub(this.position);
+			avgPos.setMag(this.maxSpeed);
+			avgPos.sub(this.velocity);	
+			avgPos.limit(this.maxForce);
+		
+			//separation
+			avgDiff.div(count);
+			avgDiff.setMag(this.maxSpeed);
+			avgDiff.sub(this.velocity);	
+			avgDiff.limit(this.maxForce);
 		}
-		return avg;
-	}
-	
-	cohesion(boids){
-		let avg = createVector();
-		let count = 0;
-		for(let b of boids){
-			let d = dist(this.position.x, this.position.y, b.position.x, b.position.y);
-			if(b != this && d <= this.perceptionRadius){
-				avg.add(b.position);
-				count ++;
-			}
-		}
-		if(count != 0){
-			avg.div(count);
-			avg.sub(this.position);
-			avg.setMag(this.maxSpeed);
-			avg.sub(this.velocity);	
-			avg.limit(this.maxForce);
-		}
-		return avg;
-	}
-	
-	separation(boids){
-		let avg = createVector();
-		let count = 0;
-		for(let b of boids){
-			let d = dist(this.position.x, this.position.y, b.position.x, b.position.y);
-			if(b != this && d <= this.perceptionRadius){
-				let diff = p5.Vector.sub(this.position, b.position);
-				diff.div(d * d);
-				avg.add(diff);
-				count ++;
-				strokeWeight(Math.min((100 * this.perceptionRadius) / (d * d), 1));
-				line(this.position.x, this.position.y, b.position.x, b.position.y);
-			}
-		}
-		if(count != 0){
-			avg.div(count);
-			avg.setMag(this.maxSpeed);
-			avg.sub(this.velocity);	
-			avg.limit(this.maxForce);
-		}
-		return avg;
-	}
-	
-	flock(boids){
+		
 		this.acceleration.set(0, 0);
+		/*
 		let alignment = this.align(boids);
 		let cohesion = this.cohesion(boids);
 		let separation = this.separation(boids);
-		/*
-		alignment.mult(alignSlider.value());
-		cohesion.mult(cohesionSlider.value());
-		separation.mult(separationSlider.value());
 		*/
-		this.acceleration.add(alignment);
-		this.acceleration.add(cohesion);
-		this.acceleration.add(separation);
+		
+		avgVel.mult(1);
+		avgPos.mult(1);
+		avgDiff.mult(1.2);
+		
+		this.acceleration.add(avgVel);
+		this.acceleration.add(avgPos);
+		this.acceleration.add(avgDiff);
+
 	}
 }
--- js/boids/sketch.js
+++ js/boids/sketch.js
@@ -1,48 +1,71 @@
 
-const boids = [];
-var cnv;
 
-let alignSlider, cohesionSlider, separationSlider;
+
+const boids = [];
+bucket = [];
+
+const perceptionRadius = 50;
+
 
 function setup(){
 	cnv = createCanvas(windowWidth, 360);
-  	//cnv.style('display', 'block');
-  	cnv.parent('js');
-  	//centerCanvas();
-  	//document.body.scrollTop = 0;
-  	//document.body.style.overflow = 'hidden';
-  	//cnv.width = document.body.clientWidth;
-    //cnv.height = document.body.clientHeight;
-	for(let i = 0; i < 100; i++){
-		boids.push(new Boid());
+	cnv.parent('js');
+	//createCanvas(720, 300);
+	for(let i = 0; i < (width / perceptionRadius); i++){
+		bucket.push([]);
+		for(let j = 0; j < (height / perceptionRadius); j++){
+			bucket[i].push([]);
+		}
+	}
+	print(bucket);
+	for(let i = 0; i < 200; i++){
+		let x = random(width);
+		let y = random(height);
+		//bucket[0][0].push(new Boid(x, y, perceptionRadius));
+		//print(parseInt(Math.min(x / perceptionRadius, (width / perceptionRadius) - 1)) + " " + parseInt(Math.min(y / perceptionRadius, (height / perceptionRadius) - 1)));
+		bucket[parseInt(Math.min(x / perceptionRadius, (width / perceptionRadius) - 1))][parseInt(Math.min(y / perceptionRadius, (height / perceptionRadius) - 1))].push(new Boid(x, y, perceptionRadius));
+		boids.push(new Boid(x, y, perceptionRadius));
 	}
 	
 }
-
-function centerCanvas() {
-    
-    cnv.position(x, y);
-}
-
-/*
-function windowResized() {
-  	centerCanvas();
-}*/
 
 function draw(){
 	background(51);
 	
+	let newBucket = [];
 	
-	
-	for(let boid of boids){
-		boid.flock(boids); 
-		boid.tick();
-		boid.draw();
+	for(let i = 0; i < bucket.length; i++){
+		newBucket.push([]);
+		for(let j = 0; j < bucket[i].length; j++){
+			newBucket[i].push([]);
+		}
 	}
+	
+	let count = 0;
+	
+	for(let i = 0; i < bucket.length; i++){
+		for(let j = 0; j < bucket[i].length; j++){	
+			for(let boid of bucket[i][j]){
+				count ++;
+				boid.flock(bucket); 
+				boid.tick();
+				boid.draw();
+				let oldPos = createVector(boid.position.x, boid.position.y);
+				let bucketXLength = bucket.length;
+				let bucketYLength = bucket[0].length;
+				let bucketX = (bucket.length + parseInt(Math.min(oldPos.x / perceptionRadius, (width / perceptionRadius) - 1))) % bucketXLength;
+				let bucketY = (bucket[0].length + parseInt(Math.min(oldPos.y / perceptionRadius, (height / perceptionRadius) - 1))) % bucketYLength;
+				newBucket[bucketX][bucketY].push(boid);
+			}
+		}
+	}
+	print(count);
 	textSize(60);
 	stroke(30);
 	textAlign(CENTER);
 	textFont('Georgia');
 	fill(255);
 	text('The Website Project', windowWidth / 2, 180);
+	bucket = newBucket;
+
 }
